34180b42b2ac4e7e6a07f187d19f3ac50364d332
musibala <musibala_timothy@gmail.com>
2023-08-16 08:04:28 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+
+
+
+
+
+#endif
